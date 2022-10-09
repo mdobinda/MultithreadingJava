60034c20fa70795eb5777aa7b787f859489b0c79
mdobinda <66760972+mdobinda@users.noreply.github.com>
2022-10-08 20:05:59 -0400

    andres

---
+++ src/project2/Conveyor.java
@@ -0,0 +1,22 @@
+package project2;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
+public class Conveyor {
+    public int ID;
+    public ReentrantLock mutex = new ReentrantLock();
+
+    public Conveyor(int ID){
+        this.ID = ID;
+    }
+
+    public void Input(int stationID){
+        System.out.printf("Routing Station %d: successfully moves packages into station on input conveyor number C%d%n", stationID, this.ID);
+    }
+
+    public void Output(int stationID){
+        System.out.printf("Routing Station %d: successfully moves packages out of station on output conveyor number C%d%n", stationID, this.ID);
+    }
+}
